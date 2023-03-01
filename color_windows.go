02ab2eaae10c365c22e6f6f2dac0d9cd496f3016
Martin Lindhe <martin@ubique.se>
2023-03-01 08:30:32 +0100

    windows: enable virtual terminal processing, fixes #169

---
+++ color_windows.go
@@ -0,0 +1,18 @@
+package color
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+func init() {
+	// Opt-in for ansi color support for current process.
+	// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#output-sequences
+	var outMode uint32
+	out := windows.Handle(os.Stdout.Fd())
+	if err := windows.GetConsoleMode(out, &outMode); err == nil {
+		outMode |= windows.ENABLE_PROCESSED_OUTPUT | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+		_ = windows.SetConsoleMode(out, outMode)
+	}
+}
